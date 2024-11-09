c218b9df7149217f852e75c1342ba39468a3018c
Mohit <mohit.peak@gmail.com>
2024-11-09 20:51:55 +0530

    Basic express server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { PORT } = require("./src/config/server.config");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
